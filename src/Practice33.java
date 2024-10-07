0b7b483d9d96582e1d74f94fc55ec606b7ae8a60
foagura <foagura_0416@yahoo.co.jp>
2024-10-07 22:17:15 +0900

    first commit

---
+++ src/Practice33.java
@@ -0,0 +1,9 @@
+public class Practice33 {
+    public static void main(String[] args) {
+        double num = 10.99;
+        int i = (int)num;    // キャストによる型変換
+
+        System.out.println("numの値は：" + num);
+        System.out.println("iの値は：" + i);
+    }
+}
