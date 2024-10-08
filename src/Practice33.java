4c57c8870077db2e6556d3111f84583bd8e42563
foagura <foagura_0416@yahoo.co.jp>
2024-10-08 11:07:20 +0900

    追加

--- src/Practice33.java
+++ src/Practice33.java
@@ -1,5 +1,13 @@
+/* 暗黙の型変換・キャストによる型変換 */
+
 public class Practice33 {
     public static void main(String[] args) {
+        byte a = 10;
+        int b = a;   // 暗黙の型変換
+
+        System.out.println("aの値は：" + a);
+        System.out.println("bの値は：" + b);
+
         double num = 10.99;
         int i = (int)num;    // キャストによる型変換
 
